0a59aa6a179fb3227080f48ad7cddb87c5e1021d
jason ma <jasonma608@gmail.com>
2022-12-09 21:20:32 -0500

    adding vueX challenge and pratice

---
+++ Module15/vuexpratice/src/components/UserAuth.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="login" v-if="!isAuth">Login</button>
+  <button @click="logout" v-if="isAuth">Logout</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    login() {
+      this.$store.dispatch('login'); //reference the methods from actions by dispatch
+    },
+    logout() {
+      this.$store.dispatch('logout');
+    }
+  },
+  computed: {
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;//getting the state of isloggedin from getters to determine which buttons
+        //needs to display
+    }
+  }
+}
+</script>
